b59fffd7ede9859f06f87204d5d17354d74ac990
Ahmed Abuelgasim <hungryphilomath@gmail.com>
2020-01-17 23:41:35 +0000

    added dummy compoenents to show global store working

---
+++ src/js/components/ChildComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useCounter } from '../store/storeApi';
+
+export const ChildComponent = (props) => {
+  const { count, message, increment, decrement, reset } = useCounter();
+
+	return (
+    <React.Fragment>
+      <h1>Hello {props.name}</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => increment()}>+</button>
+      <button onClick={() => decrement()}>-</button>
+      <button onClick={() => reset()}>Reset</button>
+      <p>{message}</p>
+    </React.Fragment>
+  );
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { StoreProvider } from './store/store';
+import { ChildComponent } from './components/ChildComponent';
 
-render(<h1>Sandbox</h1>, document.getElementById('app'));
+const App = () => {
+  return (
+    <StoreProvider>
+      <ChildComponent name="Ahmed"/>
+    </StoreProvider>
+  );
+};
+
+render(<App />, document.getElementById('app'));
