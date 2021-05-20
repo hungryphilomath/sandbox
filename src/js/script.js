4ecc9f5401f9af6f8a44b1b22194e0c8ad85eddc
Ahmed Abuelgasim <hungryphilomath@gmail.com>
2021-05-20 14:14:34 +0100

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -6,23 +6,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('load', () => {
   console.log(`${projectName}: load event`);
-
-   const playerEl = document.getElementsByTagName('video')[0];
-
-   console.log(playerEl);
-
-
-  const playerEventHandler = (e) => {
-    switch (e.type) {
-      case 'play':
-        console.log('played');
-        playerEl.addEventListener('timeupdate', playerEventHandler);
-        break;
-      case 'timeupdate':
-        console.log(Math.round(e.target.currentTime));
-        break;
-    }
-  };
-
-  playerEl.addEventListener('play', playerEventHandler, {once: true});
 });
