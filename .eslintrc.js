4d730b352f63e5529bf21ed44a8c7129ea48fda0
Ahmed Abuelgasim <ahmed@potatolondon.com>
2021-07-15 11:19:49 +0100

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,94 +2,101 @@ module.exports = {
 	'env': {
 		'browser': true,
 		'es6': true,
-		'jest': true,
 		'node': true
 	},
 	'extends': [
 		'eslint:recommended',
+		'plugin:react/recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:@typescript-eslint/recommended',
 	],
 	'globals': {
 		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
+		'SharedArrayBuffer': 'readonly',
 	},
+	'overrides': [
+		{
+			'files': [
+				'*.ts',
+				'*.tsx',
+			],
+			'rules': {
+				'@typescript-eslint/explicit-function-return-type': ['error'],
+			}
+		},
+		{
+			"files": ["*.d.ts"],
+			"rules": {
+				"no-unused-vars": 0,
+			}
+		},
+		{
+			"files": ["*[!.test].js"],
+			"rules": {
+				"@typescript-eslint/explicit-module-boundary-types": 0,
+				"indent": 0,
+			}
+		}
+	],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
+		},
 		'ecmaVersion': 2018,
-		'sourceType': 'module'
+		'sourceType': 'module',
 	},
 	'plugins': [
+		'react',
 		'@typescript-eslint',
 		'no-autofix'
 	],
 	'rules': {
 		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-explicit-any': 'warn',
+		"@typescript-eslint/no-explicit-any": "warn",
 		'arrow-spacing': [
 			'error',
 			{
+				'after': true,
 				'before': true,
-				'after': true
 			}
 		],
 		'indent': [
 			'error',
 			'tab',
 			{
-				'SwitchCase': 1
+				'SwitchCase': 1,
 			}
 		],
-		'no-autofix/prefer-const': 'error',
+		"no-autofix/prefer-const": "error",
 		'no-multiple-empty-lines': [
 			'warn',
 			{
-				'max': 3
+				'max': 3,
 			}
 		],
-		'no-unused-vars': [
-			'warn'
+		"no-unused-vars": [
+			"warn"
 		],
 		'object-curly-spacing': [
 			'error',
 			'always',
 		],
-		// 'prefer-const': 'off',
+		// "prefer-const": "off",
 		'prefer-const': [
 			'error',
 			{
-				'destructuring': 'all'
+				'destructuring': 'all',
 			}
 		],
+		'react/prop-types': 0,
 		'semi': [
 			'error',
-			'always'
+			'always',
 		],
-		'sort-imports': 'error',
-		'sort-keys': [
-			'error',
-			'asc'
+		"sort-imports": 0,
+		"sort-keys": [
+			"error",
+			"asc"
 		]
-	},
-	'overrides': [
-		{
-			'files': ['*.ts', '*.tsx'],
-			'rules': {
-				'@typescript-eslint/explicit-function-return-type': ['error']
-			}
-		},
-		{
-			'files': ['*.d.ts'],
-			'rules': {
-				'no-unused-vars': 0
-			}
-		},
-		{
-			'files': ['*[!.test].js'],
-			'rules': {
-				'indent': 0,
-				'@typescript-eslint/explicit-module-boundary-types': 0
-			}
-		}
-	]
+	}
 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import {projectName} from './common/constants.js';
+import { projectName } from './common/constants.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log(`${projectName}: DOMContentLoaded event`);
